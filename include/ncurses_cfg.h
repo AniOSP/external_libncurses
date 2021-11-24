8367e5b9b61a0e5f7954d68afe7ade179ab58377
Michael Bestas <mkbestas@lineageos.org>
2021-11-24 19:52:53 +0200

    libncurses/terminfo: Move to /product
    Change-Id: Ic387543e20bb86b629b625838e0d94ee1ec98589
    Signed-off-by: Daryl Ronningen <relms@relms.dev>

--- include/ncurses_cfg.h
+++ include/ncurses_cfg.h
@@ -63,8 +63,8 @@
 #define HAVE_LIBDL 1
 #define LIBGPM_SONAME "libgpm.so.2"
 #define HAVE_LIBGPM 1
-#define TERMINFO_DIRS "/system/etc/terminfo"
-#define TERMINFO "/system/etc/terminfo"
+#define TERMINFO_DIRS "/product/etc/terminfo"
+#define TERMINFO "/product/etc/terminfo"
 #define HAVE_BIG_CORE 1
 #define PURE_TERMINFO 1
 #define USE_HOME_TERMINFO 1
